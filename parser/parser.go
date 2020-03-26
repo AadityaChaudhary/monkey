0c0c4865d6ed52d1a3fd00c0729f880bb454612c
Aaditya Chaudhary <1chaudharaad@hdsb.ca>
2020-03-26 01:14:46 -0400

    added in basic lets statement parsing

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,66 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		statement := p.ParseStatement()
+
+		if statement != nil {
+			program.Statements = append(program.Statements,statement)
+		}
+		p.nextToken()
+	}
+
+	return program
+
+}
+
+func(p *Parser) ParseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.ParseLetStatement()
+	default:
+		return nil
+	}
+
+}
+
+func(p *Parser) ParseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.currIs(token.EOF) {
+		p.nextToken()
+	}
+
+	return statement
+
+}
+
+func(p *Parser) peekIs(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
+}
+
+func(p *Parser) currIs(tokenType token.TokenType) bool {
+	return p.curToken.Type == tokenType
+}
+
+func(p *Parser) expectPeek(tokenType token.TokenType) bool {
+	if p.peekToken.Type == tokenType {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
